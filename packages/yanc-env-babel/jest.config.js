5af90123ff6041ffbb355da4a2cc0cf4eda6f8ce
peterk <tiglek+u16office@gmail.com>
2021-09-02 19:03:15 +0900

    rename _src to src

--- packages/yanc-env-babel/jest.config.js
+++ packages/yanc-env-babel/jest.config.js
@@ -1,12 +1,12 @@
 // jest.config.js
-//const pathconfig = require("./pathconfig.json");
+const pathconfig = require("./pathconfig.json");
 
 module.exports = {
   // module resolution
-  //  rootDir: ".",
-  // moduleNameMapper: {
-  //   ...pathconfig.jest,
-  // },
+  rootDir: ".",
+  moduleNameMapper: {
+    ...pathconfig.jest,
+  },
   // test env
   testEnvironment: "node",
   transform: {
