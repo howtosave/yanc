cbacc3c99401934f8316841fcb658aaba7816171
peterk <tiglek+u16office@gmail.com>
2021-09-13 17:06:12 +0900

    working on babel and core-js

--- examples/env-babel/src/hello-my-world.ts
+++ examples/env-babel/src/hello-my-world.ts
@@ -3,6 +3,8 @@ import my from "~/src/lib/my";
 import world from "~/lib/world";
 import nsworld from "~/lib/namespace-world";
 
-console.log(">>>", hello(), my(), world(), nsworld());
+const sym = Symbol();
+
+console.log(">>>", hello(), my(), world(), nsworld(), sym.toString());
 // export libs
 export { hello, my, world };
